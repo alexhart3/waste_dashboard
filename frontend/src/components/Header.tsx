5331381ee6c2df39801ab3a9da50337a75ea4cbd
Alexander Hartigan <alexanderhartigan3@gmail.com>
2026-09-18 11:40:03 -0700

    Add header component

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,11 @@
+export default function Header() {
+    return (
+        <header className="top">
+            <div className="brand">
+                <div>
+                    <h1>Waste Collection Dashboard</h1>
+                </div>
+            </div>
+        </header>
+    )
+}
